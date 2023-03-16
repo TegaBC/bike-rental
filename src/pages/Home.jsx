62c6a5c9cf199bde0e6a080f19b8ac7024bf7d7c
TegaBC <64938182+TegaBC@users.noreply.github.com>
2023-03-16 01:12:00 +0000

    locally saved images

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -12,10 +12,10 @@ const navLinks = [
 ]
 
 const fakeData = [
-    {name: "DiamondBack", image: "https://www.silvesterbrothers.com/images/raleigh/snc114r.jpg", hardtail: true, size: 20, beginner: false, cost: 35},
-    {name: "Forme", image: "https://www.silvesterbrothers.com/images/black-rocks-ht2-blue-_product-image-1__3-2.png", hardtail: true, size: 26, beginner: true, cost: 15},
-    {name: "Trek", image: "https://www.balfesbikes.co.uk/images/imageedit_1_2631292913.jpg", hardtail: true, size: 24, beginner: false, cost: 20},
-    {name: "Vitus", image: "https://www.wigglestatic.com/product-media/106241389/Vitus-Escarpe-27-CRS-Mountain-Bike-2022-oryx-grey-01.jpg?w=1200&h=1200&a=7", hardtail: false, size: 28, beginner: false, cost: 25},
+    {name: "DiamondBack", image: "src/images/bikes/db.jpg", hardtail: true, size: 20, beginner: false, cost: 35},
+    {name: "Forme", image: "src/images/bikes/forme.png", hardtail: true, size: 26, beginner: true, cost: 15},
+    {name: "Trek", image: "src/images/bikes/trek.jpg", hardtail: true, size: 24, beginner: false, cost: 20},
+    {name: "Vitus", image: "src/images/bikes/vitus.jpg", hardtail: false, size: 28, beginner: false, cost: 25},
   ]
 
 function home() {
