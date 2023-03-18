bf9b51f27775bf06b7f9e516b53fc92ef8148923
TegaBC <64938182+TegaBC@users.noreply.github.com>
2023-03-18 02:03:19 +0000

    adding routing and nav

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,19 @@
 import "./App.css"
 import HomePage from "./pages/Home"
+import AboutPage from "./pages/About"
+import { Routes, Route } from "react-router-dom"
+import Navbar from "./components/Navbar"
+
 
 function App() {
 
-  return (
-    <div className="App">
-        <HomePage />
-    </div>
-  )
+  return <>
+    <Navbar />
+    <Routes>
+        <Route path="/" element={<HomePage/>}/>
+        <Route path="/about" element={<AboutPage/>}/>
+  </Routes>
+  </>
 }
 
 export default App
