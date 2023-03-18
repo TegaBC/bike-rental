5433059afacf1db23e6a0396dfb9f4df4c4d5671
TegaBC <64938182+TegaBC@users.noreply.github.com>
2023-03-18 00:41:05 +0000

    init footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import Styles from "../styles/Footer.module.css"
+
+function Footer() {
+    return(
+        <div className={Styles.footer}>
+            © TegaBC | 2023
+        </div>
+    )
+}
+
+export default Footer
