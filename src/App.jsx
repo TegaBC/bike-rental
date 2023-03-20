10262fa2bbd1fbc6ebe8818e1da25d0152212520
TegaBC <jjbencarew10@gmail.com>
2023-03-20 10:10:58 +0000

    adding new page routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import "./App.css"
 import HomePage from "./pages/Home"
 import AboutPage from "./pages/About"
+import TestimonialPage from "./pages/TestimonialPage"
 import { Routes, Route } from "react-router-dom"
 import Navbar from "./components/Navbar"
 
@@ -11,6 +12,7 @@ function App() {
     <Routes>
         <Route path="/" element={<HomePage/>}/>
         <Route path="/about" element={<AboutPage/>}/>
+        <Route path="/testimonials" element={<TestimonialPage/>}/>
     </Routes>
   </>
 }
